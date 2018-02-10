c0e87f8f483589d65f9f99f452366e8fe40e2956
Taric Jain <taricjain@outlook.com>
2018-02-09 23:53:39 -0600

    autocomplete works

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, NgZone } from '@angular/core';
 import { WeatherService } from '../weather.service';
+import { FormControl, FormsModule } from '@angular/forms';
+import { MapsAPILoader, AgmCoreModule } from '@agm/core';
+import {} from 'googlemaps';
 
 @Component({
   selector: 'app-content',
@@ -9,10 +12,57 @@ import { WeatherService } from '../weather.service';
 export class ContentComponent implements OnInit {
   @Input()
   title: string;
-  
-  constructor(private weatherService: WeatherService) { }
+
+  public latitude: number;
+  public longitude: number;
+  public zoom: number;
+  public searchControl: FormControl;
+
+  @ViewChild("search")
+  public searchElementRef: ElementRef;
+
+  constructor(private mapsAPILoader: MapsAPILoader, private ngZone: NgZone ) {}
+
 
   ngOnInit() {
+    //default overlook on America
+    this.latitude = 39.8282;
+    this.longitude = -98.5795;
+    this.zoom = 4;
+    
+    this.searchControl = new FormControl;
+    this.setCurrentPosition();
+
+    this.mapsAPILoader.load().then(() => {
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+        types: ["geocode"]
+      });
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+  
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+          
+          //set latitude, longitude and zoom
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.zoom = 12;
+        });
+      });
+    });
+  }
+  private setCurrentPosition() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) =>{
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 12;
+      });
+    }
   }
 
 }
