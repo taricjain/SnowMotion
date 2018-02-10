f290db51bc6b7ff70599f5930f6cc09ce1bc2d36
Sai Kiran Dasika <kirandasika30@gmail.com>
2018-02-10 00:01:49 -0600

    validation to service method

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -8,8 +8,13 @@ export class WeatherService {
 
   constructor(private httpService: HttpClient) { }
 
-  getDataWithCoordinates(callback: CallbackInterface) {
-    var serverPayload: ServerPayload = { api_url: "https://api.darksky.net/forecast/5bac16c1f3e50319994bb24e50ec5ec5/32.593357,-85.495163?exclude=[flags,alerts]" };
+  getDataWithCoordinates(coordinate: Coordinate, callback: CallbackInterface) {
+    if (coordinate === undefined || coordinate === null) {
+      callback(new Error("coordinate needed for service call."));
+    }
+    var serverPayload: ServerPayload = {
+      api_url: "https://api.darksky.net/forecast/5bac16c1f3e50319994bb24e50ec5ec5/32.593357,-85.495163?exclude=[flags,alerts]"
+    };
     this.httpService.post(environment.SERVER_API_URL, serverPayload)
     .subscribe((data) => callback(null, data));
   }
