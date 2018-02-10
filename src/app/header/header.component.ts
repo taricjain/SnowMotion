4c2f23efb0a13d52f8e0fecc7e8c0acfb47e1a59
2dhruvpatel <dsp0017@auburn.edu>
2018-02-10 02:50:04 -0600

    sidenav
    sidenav

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -14,6 +14,6 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  showFiller = false;
 
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { MatCardModule, MatButtonModule, MatToolbarModule, MatIconModule, MatInputModule, MatFormFieldModule } from '@angular/material';
+import { MatCardModule, MatButtonModule, MatToolbarModule, MatIconModule, MatInputModule, MatFormFieldModule, MatSidenavModule } from '@angular/material';
 
 @NgModule({
   imports: [
@@ -12,9 +12,10 @@ import { MatCardModule, MatButtonModule, MatToolbarModule, MatIconModule, MatInp
     MatToolbarModule,
     MatIconModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatSidenavModule
   ],
-  exports: [MatCardModule, MatButtonModule, MatToolbarModule, MatIconModule, MatInputModule, MatFormFieldModule],
+  exports: [MatCardModule, MatButtonModule, MatToolbarModule, MatIconModule, MatInputModule, MatFormFieldModule, MatSidenavModule],
   declarations: []
 })
 export class MaterialModule { }
