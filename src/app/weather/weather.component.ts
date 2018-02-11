73820161e85ba0cc140fbf9d61aee0cf78888e6f
Taric Jain <taricjain@outlook.com>
2018-02-11 11:52:37 -0600

    ui changes

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -115,4 +115,11 @@ export class WeatherComponent implements OnInit, OnChanges {
     });
   }
 
+  tiles = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
+
 }
